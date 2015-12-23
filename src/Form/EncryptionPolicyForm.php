0e91b8310b2dc4559d79d5ac20182e8ee7d84571
Bryan Hirsch <bryan@bryanhirsch.com>
2015-12-22 22:55:44 -0500

    Updated EncryptionPolicyForm.
      Includes admin config for:
      - cipher_suites_available
      - cipher_suites_blacklist
      - cipher_suites_whitelist
    
     	modified:   src/Form/EncryptionPolicyForm.php

--- src/Form/EncryptionPolicyForm.php
+++ src/Form/EncryptionPolicyForm.php
@@ -38,8 +38,6 @@ class EncryptionPolicyForm extends ConfigFormBase {
    */
   public function __construct(ConfigFactoryInterface $config_factory) {
     parent::__construct($config_factory);
-
-    $this->allCiphers = include('../../encryption_policy.all_suites.php');
   }
 
   /**
@@ -60,26 +58,119 @@ class EncryptionPolicyForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $ciphers = $this->getCiphers();
+    $cipher_suites           = $this->settingsToString( $this->getCipherSuites() );
+    $cipher_suites_blacklist = $this->settingsToString( $this->getCipherSuitesBlacklist() );
+    $cipher_suites_whitelist = $this->settingsToString( $this->getCipherSuitesWhitelist() );
 
-    // Make keys and values match.
-    $options = array_combine($ciphers, $ciphers);
+    // Define a list of cipher suites available on your server.
+    $form['available'] = array(
+      '#type' => 'details',
+      '#title' => t('Cipher suites available'),
+      '#description' => t('Enter a list of available cipher suites supported by your server here. Wherever your TLS/SSL connection terminates... @TODO finish explaination here.'),
+    );
+    $form['available']['cipher_suites_available'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Cipher suites available on your server'),
+      '#default_value' => $cipher_suites,
+      '#description' => t('Defaults to a full list of TLS cipher suites'),
+    );
 
-    $form['encryption_policy']['ciphers'] = array(
-      '#type' => 'checkboxes',
-      '#options' => $options,
+    // Provide some helpful info about available cipher suites for users who
+    // need help figuring out what to enter for the cipher_suites_available
+    // setting.
+    $form['available']['cipher_suite_check_link'] = array(
+      '#type' => 'item',
+      '#title' => '@TODO Enter links to sites that do checks here',
+      '#description' => t('desc'),
+    );
+    $form['available']['cipher_suite_check_script'] = array(
+      '#type' => 'item',
+      '#title' => '@TODO Enter bash script here',
+      '#description' => t('desc'),
+    );
+
+    // Blacklist cipher suites if there are any supported on your server that
+    // you want to explicitly disallow.
+    $form['blacklist'] = array(
+      '#type' => 'details',
+      '#title' => t('Cipher suites blacklist'),
+      '#description' => t('@TODO'),
+    );
+    $form['blacklist']['cipher_suites_blacklist'] = array(
+      '#type' => 'textarea',
+      '#default_value' => $cipher_suites_blacklist,
+      '#title' => t('Options'),
+    );
+
+    // Whitelist cipher suites if you only want to allow an explicit subset
+    // of cipher suites supported by your server.
+    $form['whitelist'] = array(
+      '#type' => 'details',
+      '#title' => t('Cipher suites whitelist'),
+      '#description' => t('@TODO'),
+    );
+    $form['whitelist']['cipher_suites_whitelist'] = array(
+      '#type' => 'textarea',
+      '#default_value' => $cipher_suites_whitelist,
       '#title' => t('Options'),
     );
 
     return parent::buildForm($form, $form_state);
   }
 
-  private function getCiphers() {
-    // If specific list of server-side ciphers is available, use that.
-    // @TODO
+  /**
+   * Return array of cipher suite names.
+   *
+   * This list may correspond to cipher suites available on the server where the
+   * TLS/SSL connection terminates, or it may just be a list of all TLS cipher
+   * suites. We'll use the best info available.
+   *
+   * @return array
+   */
+  private function getCipherSuites() {
+    // Use a specific list of available server-side ciphers if we have this.
+    $cipher_suites_available = $this->getAvailableCipherSuites();
+    if (count($cipher_suites_available) > 0) {
+      return $cipher_suites_available;
+    }
 
     // Otherwise use full list of TLS Ciphers.
     return $this->getAllTlsCipherSuites();
+  }
+
+  /**
+   * Return an array of available server-side cipher suites.
+   *
+   * @return array
+   *   Array is empty if no information is available.
+   */
+  private function getAvailableCipherSuites() {
+    $ciphers = $this->config('encryption_policy.settings')->get('cipher_suites_available');
+    return $this->settingsToArray($ciphers);
+  }
+
+  /**
+   * Check type of $settings. Convert to empty array if not set as array yet.
+   *
+   * @param $settings
+   * @return array
+   */
+  private function settingsToArray($settings) {
+    $type = gettype($settings);
+
+    if ($type == 'array') {
+      return $settings;
+    }
+
+    if (empty($settings)) {
+      return array();
+    }
+
+    return explode("\r\n", $settings);
+  }
+
+  private function settingsToString(array $settings) {
+    return implode("\r\n", $settings);
   }
 
   /**
@@ -424,7 +515,7 @@ class EncryptionPolicyForm extends ConfigFormBase {
       "TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
 
       // http://tools.ietf.org/html/draft-ietf-tls-56-bit-ciphersuites-01
-      "TLS_RSA_EXPORT1024_WITH_DES_CBC_SHA",
+
       "TLS_DHE_DSS_EXPORT1024_WITH_DES_CBC_SHA",
       "TLS_RSA_EXPORT1024_WITH_RC4_56_SHA",
       "TLS_DHE_DSS_EXPORT1024_WITH_RC4_56_SHA",
@@ -432,13 +523,30 @@ class EncryptionPolicyForm extends ConfigFormBase {
     );
   }
 
+  private function getCipherSuitesBlackList() {
+    $ciphers = $this->config('encryption_policy.settings')->get('cipher_suites_blacklist');
+    return $this->settingsToArray($ciphers);
+  }
+
+  private function getCipherSuitesWhitelist() {
+    $ciphers = $this->config('encryption_policy.settings')->get('cipher_suites_whitelist');
+    return $this->settingsToArray($ciphers);
+  }
+
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->config('encryption_policy.settings')
-      ->set('element', $form_state['values']['encryption_policy']['ciphers'])
-      ->save();
+    $values = $form_state->getValues();
+
+    $variables = array('available', 'blacklist', 'whitelist');
+    foreach ($variables as $variable) {
+      $key = 'cipher_suites_' . $variable;
+      $settings = $this->settingsToArray($values[$key]);
+      $this->config('encryption_policy.settings')
+        ->set($key, $settings)
+        ->save();
+    }
 
     parent::submitForm($form, $form_state);
   }
