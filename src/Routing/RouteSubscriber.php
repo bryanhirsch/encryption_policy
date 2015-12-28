b9500ba0918059a15d1d19321b632cf5a62e0f31
Bryan Hirsch <bryan@bryanhirsch.com>
2015-12-28 10:24:17 -0500

    Unfinished. Getting this to play nicely with password_policy is not MVP.

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -8,7 +8,9 @@
 namespace Drupal\encryption_policy\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
+// use  \Drupal\Core\Routing\RouteProvider;
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
 
 /**
  * Class RouteSubscriber.
@@ -17,9 +19,53 @@ use Symfony\Component\Routing\RouteCollection;
  * Listens to the dynamic route events.
  */
 class RouteSubscriber extends RouteSubscriberBase {
+  private $routeProvider;
+
+  // @TODO Use dependency injection. Then use RouteProvider::getRoutesByPattern() to get path in alter routes below.
+  // private function __construct(RouteProvider $routeProvider) {
+  public function __construct() {
+    $routeProvider = \Drupal::service('router.route_provider');
+    $this->routeProvider = $routeProvider;
+  }
+  // */
+
   /**
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
+
+    // Check if some other module (e.g. password_policy) has already provided /admin/config/security.
+    if ($this->pathExists('/admin/config/security')) {
+      $collection->remove('enryption_policy.admin_index');
+    }
+
+    // Provide security "home" page.
+    $path = '/admin/config/security';
+    $defaults = array(
+      '_controller' => '\Drupal\system\Controller\SystemController::systemAdminMenuBlockPage',
+      '_title' => 'Security',
+    );
+    $requirements = array(
+        '_permission' => 'access administration pages'
+    );
+    $route = new Route($path, $defaults, $requirements);
+    $collection->add('encryption_policy.admin_index', $route);
   }
+
+  /**
+   * Determine if requested path exists.
+   *
+   * @return bool
+   */
+  protected function pathExists($path, $debug = FALSE) {
+    $route_collections = $this->routeProvider->getRoutesByPattern($path);
+
+    if ($debug) {
+      print_r($route_collections);
+    }
+
+    $path_exists = (count($route_collections) > 1) ? TRUE : FALSE;
+    return $path_exists;
+  }
+
 }
