c50d51b254a35dc5f6ce665a510441f91fd82c16
Bryan Hirsch <bryan@bryanhirsch.com>
2015-12-28 16:18:48 -0500

    WORK IN PROGRESS: theming check in browser

--- encryption_policy.js
+++ encryption_policy.js
@@ -1,8 +1,24 @@
-function reqListener () {
-  console.log(this.responseText);
-}
+(function () {
+  'use strict';
+  // Custom javascript
+  function requestListener () {
+    var response = JSON.parse(this.responseText);
+    console.log("This website is testing your browser's encryption capabilities here: https://www.howsmyssl.com/a/check. This is the response received:");
+    console.log(this.responseText);
+    console.log(response);
 
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "https://www.howsmyssl.com/a/check");
-oReq.send();
+    Drupal.theme('');
+  }
+
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", requestListener);
+  oReq.open("GET", "https://www.howsmyssl.com/a/check");
+  oReq.send();
+
+  Drupal.theme.encryptionPolicyCheck = function(result) {
+    var output = '<h1>hello world</h1>';
+    console.log('HELLO WORLD');
+    console.log(result);
+    return output;
+  }
+})();
